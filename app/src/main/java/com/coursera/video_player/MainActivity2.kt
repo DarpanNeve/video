d83df5756b388c8ba1312b26bbc8ef0061a9f5f9
Darpan Neve <darpanneve3@gmail.com>
2023-06-09 19:42:24 +0530

    minute changes

--- app/src/main/java/com/coursera/video_player/MainActivity2.kt
+++ app/src/main/java/com/coursera/video_player/MainActivity2.kt
@@ -1,11 +1,26 @@
 package com.coursera.video_player
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val videoview =findViewById<VideoView>(R.id.videoView)
+        val mediaController=MediaController(this)
+        mediaController.setAnchorView(videoview)
+        val uri:Uri= Uri.parse("android.resource://$packageName/raw/demo_1")
+        videoview.setMediaController(mediaController)
+        videoview.setVideoURI(uri)
+        videoview.requestFocus()
+        videoview.start()
+
+
     }
 }
